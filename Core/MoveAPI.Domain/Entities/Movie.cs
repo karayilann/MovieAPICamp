2168308d47b281dd38055f97e8d3f1fa96799984
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-02-19 01:46:42 +0300

    Added Some Entitites

---
+++ Core/MoveAPI.Domain/Entities/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveAPI.Domain.Entities
+{
+    class Movie
+    {
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        public string CoverImageURL { get; set; }
+        public decimal Rating { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string CreatedYear { get; set; }
+        public bool Status { get; set; }
+    }
+}
